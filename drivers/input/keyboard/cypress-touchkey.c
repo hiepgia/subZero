a704bea60d86a9d0208165103266f3a3dce6a805
Lowell Tyler <mrpsycho.xda@gmail.com>
2012-04-03 23:54:24 -1000

    misc: added ezekeel's touchwake (mialwe/derteufel)

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -445,11 +445,11 @@ static void cypress_touchkey_enable_led_notification(void){
   }
   else
 #ifdef CONFIG_TOUCH_WAKE
-      {
+  {
     enable_touchkey_backlights();
-      }
+  }
 #else
-      pr_info("%s: cannot set notification led, touchkeys are enabled\n",__FUNCTION__);
+  pr_info("%s: cannot set notification led, touchkeys are enabled\n",__FUNCTION__);
 #endif
 }
 
@@ -474,10 +474,9 @@ static void cypress_touchkey_disable_led_notification(void){
     #endif
   }
 #ifdef CONFIG_TOUCH_WAKE
-  else
-      {
+  else {
     disable_touchkey_backlights();
-      }
+  }
 #endif  
 }
 
