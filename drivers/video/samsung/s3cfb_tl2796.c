199115e2b858127573c7a007862162d36a029d64
Lowell Tyler <mrpsycho.xda@gmail.com>
2012-04-25 16:09:52 -1000

    Revert "video: change voodoo color to be controlled by ROM Control"
    This reverts commit 561f174e0f3f56fe6bda9b62e542db158626e324.

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -1100,7 +1100,7 @@ static struct attribute_group voodoo_color_group = {
 
 static struct miscdevice voodoo_color_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "samoled_color",
+	.name = "voodoo_color",
 };
 #endif
 
